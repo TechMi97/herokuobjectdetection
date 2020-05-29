3e017230e9d1faab8853a1c9166bb0c5acc01a12
Tayub <mdtayub97@gmail.com>
2020-05-30 00:25:24 +0800

    0.2

--- ObjectDetector.py
+++ ObjectDetector.py
@@ -39,7 +39,7 @@ class Detector:
 
         for i in range(detections.shape[2]):
             confidence = detections[0, 0, i, 2]
-            if confidence > 0.3:
+            if confidence > 0.2:
                 class_id = int(detections[0, 0, i, 1])
 
                 xLeftBottom = int(detections[0, 0, i, 3] * cols)
